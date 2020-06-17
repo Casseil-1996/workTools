ea73780ea83e725e69cab477d4d05dd95f159598
xuhao <xuhao@eastfair.com>
2020-06-17 17:51:25 +0800

    06/17

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <div id="app">
+  <div id="app" :style="`background-image: url(${bg})`">
     <router-view />
   </div>
 </template>
 
 <script>
+import bg from '../public/img/star-bg.svg'
 export default {
   name: "app",
+  data () {
+    console.log(bg)
+    return { bg }
+  },
   components: {}
 };
 </script>
 
-<style>
+<style lang="less" scoped>
+#app {
+  background-size: contain;
+  background-position: center;
+}
 </style>
